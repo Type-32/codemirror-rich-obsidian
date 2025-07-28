fa09ecef6f7623a6350e18ced7be1910d4683c0f
Wilson Su <wilsonsu2008@hotmail.com>
2025-07-28 10:16:07 +0800

    init: init

---
+++ app/components/Editor.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+@reference "~/assets/css/main.css";
+
+div {
+    font-family: var(--font-editor), sans-serif;
+}
+</style>
